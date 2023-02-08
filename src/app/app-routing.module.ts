41fa6844d4b6b79bf5835a878518b4639dbe05b5
Arlyule <torresreyessamel2@gmail.com>
2023-02-08 10:59:29 -0600

    Clase-01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:PrincipalComponent},
+  {path: 'login',component:LoginComponent},
+  {path: 'register',component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
