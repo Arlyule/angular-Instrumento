a0fc2c90f7d31170d51c1edc011619cea2b80b5c
Arlyule <105952154+Arlyule@users.noreply.github.com>
2023-02-17 19:46:55 -0600

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { RegisterComponent } from './components/register/register.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
@@ -17,23 +16,8 @@ import { AuthModule } from '@auth0/auth0-angular';
     RegisterComponent,
     PrincipalComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserModule,
-
-    // Import the module into the application, with configuration
-    AuthModule.forRoot({
-      domain: 'dev-kwjpz2fofl0gmdkc.us.auth0.com',
-      clientId: 'iq2pMeeJgeVcj0lnd5XgutpOlE4k30MZ',
-      authorizationParams: {
-        redirect_uri: window.location.origin
-      }
-    }),
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
